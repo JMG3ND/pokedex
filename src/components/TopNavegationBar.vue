c9254833df3f36a9ad77e6e0b2bffb9c80e2cef0
José Gutiérrez <jo0303se@hotmail.com>
2023-04-09 13:56:57 -0500

    commit

---
+++ src/components/TopNavegationBar.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <header class="top-navegation-bar">
+        
+        <RouterLink to="/home">Home</RouterLink>
+    </header>
+</template>
+
+<style lang="scss">
+.top-navegation-bar{
+    max-width: 1500px;
+    margin: 0 auto;
+}
+</style>
