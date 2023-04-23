ddb1685d24c62a19860d82a77a180e2d459d4dec
José Gutiérrez <jo0303se@hotmail.com>
2023-04-23 17:01:36 -0500

    commit

--- src/composables/getData.js
+++ src/composables/getData.js
@@ -1,12 +1,8 @@
-//Archivo js para hacer consultas a los servidores
-
 import axios from "axios";
 import { ref } from "vue";
-//import { useStoreTier } from '../store/TierPokemmo'
 
 export const useGetData = () => {
     const data = ref(null);
-    //const { tierOU } = useStoreTier();
 
     const getData = async (url) => {
         try {
@@ -17,20 +13,8 @@ export const useGetData = () => {
         }
     };
 
-    //Esta función obtiene de la api los pokemones registrados como OU dentro del store TierPokemmo
-    /*const getDataOU = async (url) => {
-        try {
-            const answer = await axios.get(url);
-            answer.data.results = answer.data.results.filter((element, index) => tierOU.includes(index + 1));
-            data.value = answer.data;
-        } catch (error) {
-            console.log(error);
-        }
-    }*/
-
     return {
         data,
         getData,
-        //getDataOU,
     }
 }
